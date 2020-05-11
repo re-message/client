9a9d53fc7ae82ce43b13535ba421b163417a96fc
Oleg Kozlov <h1karo@outlook.com>
2020-05-11 11:55:48 +0300

    Add properties to user entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,6 +2,8 @@
 
 namespace RM\Component\Client\Entity;
 
+use DateTime;
+use DateTimeInterface;
 use RM\Component\Client\Annotation\Entity;
 use RM\Component\Client\Repository\UserRepository;
 
@@ -16,10 +18,24 @@ use RM\Component\Client\Repository\UserRepository;
 class User implements CreatableFromArray, Identifiable
 {
     private string $id;
+    private ?string $phone;
+    private string $firstName;
+    private ?string $lastName;
+    private bool $active = true;
+    private DateTimeInterface $birthday;
 
-    public function __construct(string $id)
-    {
+    public function __construct(
+        string $id,
+        ?string $phone,
+        string $firstName,
+        ?string $lastName,
+        DateTimeInterface $birthday
+    ) {
         $this->id = $id;
+        $this->phone = $phone;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->birthday = $birthday;
     }
 
     /**
@@ -30,11 +46,52 @@ class User implements CreatableFromArray, Identifiable
         return $this->id;
     }
 
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function getFullName(): string
+    {
+        $pieces = [$this->getFirstName(), $this->getLastName()];
+        return implode(' ', array_filter($pieces));
+    }
+
+    public function __toString(): string
+    {
+        return $this->getFullName();
+    }
+
+    public function isActive(): bool
+    {
+        return $this->active;
+    }
+
+    public function getBirthday(): DateTimeInterface
+    {
+        return $this->birthday;
+    }
+
     /**
      * @inheritDoc
      */
     public static function createFromArray(array $array): self
     {
-        return new self($array['id']);
+        $id = $array['id'];
+        $phone = $array['phone'] ?? null;
+        $firstName = $array['firstName'];
+        $lastName = $array['lastName'] ?? null;
+        $birthday = DateTime::createFromFormat('U', $array['birthday']);
+        return new self($id, $phone, $firstName, $lastName, $birthday);
     }
 }
