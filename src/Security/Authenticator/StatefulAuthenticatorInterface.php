bca71fc6ed163ab5587d33ee16612a654cd88177
Oleg Kozlov <h1karo@outlook.com>
2020-06-01 20:03:43 +0300

    Implement stateful authenticator interface

---
+++ src/Security/Authenticator/StatefulAuthenticatorInterface.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace RM\Component\Client\Security\Authenticator;
+
+/**
+ * Interface StatefulAuthenticatorInterface
+ *
+ * @package RM\Component\Client\Security\Authenticator
+ * @author  Oleg Kozlov <h1karo@outlook.com>
+ */
+interface StatefulAuthenticatorInterface extends AuthenticatorInterface
+{
+    /**
+     * Saves the state of authenticator in storage.
+     *
+     * @return static
+     */
+    public function store(): self;
+
+    /**
+     * Restore state of authenticator from storage.
+     *
+     * @return static
+     */
+    public function restore(): self;
+
+    /**
+     * Clears state of authenticator.
+     * This method can clear the state stored in the storage.
+     *
+     * @return static
+     */
+    public function clear(): self;
+}
