0e56319093d8371ca85fdb269f95c7f3324071c3
Oleg Kozlov <h1karo@relmsg.ru>
2020-08-16 14:48:05 +0300

    Revert "Remove client configurator"
    This reverts commit 9f0cf9a5

---
+++ src/ClientConfigurator.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * This file is a part of Relations Messenger Client.
+ * This package is a part of Relations Messenger.
+ *
+ * @link      https://github.com/relmsg/client
+ * @link      https://dev.relmsg.ru/packages/client
+ * @copyright Copyright (c) 2018-2020 Relations Messenger
+ * @author    Oleg Kozlov <h1karo@relmsg.ru>
+ * @license   https://legal.relmsg.ru/licenses/client
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RM\Component\Client;
+
+use RM\Component\Client\Transport\ThrowableTransport;
+use RM\Component\Client\Transport\TransportInterface;
+
+/**
+ * Class ClientConfigurator.
+ *
+ * @author Oleg Kozlov <h1karo@relmsg.ru>
+ */
+class ClientConfigurator
+{
+    private TransportInterface $transport;
+    private bool $throwable = true;
+
+    public function __construct(TransportInterface $transport)
+    {
+        $this->transport = $transport;
+    }
+
+    public static function create(TransportInterface $transport): self
+    {
+        return new static($transport);
+    }
+
+    public function setThrowable(bool $throwable): ClientConfigurator
+    {
+        $this->throwable = $throwable;
+
+        return $this;
+    }
+
+    public function build(): ClientInterface
+    {
+        $transport = $this->transport;
+        if ($this->throwable && !$transport instanceof ThrowableTransport) {
+            $transport = new ThrowableTransport($transport);
+        }
+
+        return ClientFactory::create($transport)->build();
+    }
+}
